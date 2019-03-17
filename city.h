582adba165ba1be529b7403ec3ebf366658a5d57
Isaac Fu <isaacfu@169-231-156-76.wireless.ucsb.edu>
2019-03-16 21:03:09 -0700

    added comments in code and some things in sim.cpp

--- city.h
+++ city.h
@@ -13,21 +13,25 @@ class City{
     City();
     ~City();
 
+    // Setters and Getters
     int getCityNo(){return cityNo;}
     void setPercent(int newPercent){percent = newPercent;};
     int getPercent(){return percent;}
     void setCityNo(int newCityNo){cityNo = newCityNo;} // update/assign the current City ID
-
     std::vector<User> getUsers(){return Users;}
     int setAdjMatrix(std::vector< std::vector<int> > desiredMatrix);
     int getDistance(int cityID); // return the distance between this city and targetCity
     void updateDistance(int distance, City targetCity); // update the distance between this city and targetCity
 
+    // Member functions
+    // Adding another city and updating adjMatrix for the distance to that city
     void addCity(int distance[]);
+    // Removing a user from list of Users
     void removeUser(User targetUser);
+    // Adding a user to the city
     void addUser(User targetUser);
+    // Printing for testing purposes
     void printUsers();
-
     void printAdjMatrix();
 
   private:
